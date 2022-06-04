5f00b876ced4157f18036182d153e552b208d972
petushka1 <nadya@devery.io>
2022-06-04 08:49:13 +0300

    edit linters errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
 import loadTaskManager from './app.js';
-import extendModule from './extend.js'
+// import extendModule from './extend.js';
 
 window.addEventListener('DOMContentLoaded', loadTaskManager);
